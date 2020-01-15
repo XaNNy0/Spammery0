c22bdfae61ecf5b98d273fd92eab559fb2987ddb
Rico Kraft <rico3004@gmx.de>
2020-01-15 21:12:51 +0100

    See comments for fewer info Or call/write me

---
+++ Net Framework/KeySpammer/KeySpammer/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using WindowsInput;
+using WindowsInput.Native;
+
+namespace KeySpammer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IInputSimulator simulator = new InputSimulator();
+            IKeyboardSimulator keyboard = new KeyboardSimulator(simulator);
+
+            //Hier gewünschten Key einfügen
+            //keyboard.KeyPress(VirtualKeyCode.VK_1);
+
+            //Für Fließtext
+            keyboard.TextEntry("Automatisch generiert! \n");
+
+            //Hier gewünschten Timeout in millisec einfügen
+            keyboard.Sleep(3000);
+
+            //Nutze ein while(true) loop für unbegrenzte Macht!
+            //Verwenden von Command Line Arguments (args) um Anwendung beim Starten zu konfigurieren!
+        }
+    }
+}
